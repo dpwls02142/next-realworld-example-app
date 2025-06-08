47bdf0155ae590f2006e0fe85fae40ad0419ab12
dpwls <dpwls02142@gmail.com>
2025-06-08 14:56:34 +0900

    refactor: 렌더링 컴포넌트 분리

--- pages/article/[slug].tsx
+++ pages/article/[slug].tsx
@@ -10,6 +10,39 @@ import { Article } from '../../lib/types/articleType';
 import { SERVER_BASE_URL } from '../../lib/utils/constant';
 import fetcher from '../../lib/utils/fetcher';
 
+const ArticleBanner = ({ title, article }) => (
+  <div className="banner">
+    <div className="container">
+      <h1>{title}</h1>
+      <ArticleMeta article={article} />
+    </div>
+  </div>
+);
+
+const ArticleContent = ({ htmlContent }) => (
+  <div className="col-xs-12">
+    <div dangerouslySetInnerHTML={htmlContent} />
+  </div>
+);
+
+const TagList = ({ tags }) => (
+  <ul className="tag-list">
+    {tags.map((tag) => (
+      <li key={tag} className="tag-default tag-pill tag-outline">
+        {tag}
+      </li>
+    ))}
+  </ul>
+);
+
+const CommentsSection = () => (
+  <div className="row">
+    <div className="col-xs-12 col-md-8 offset-md-2">
+      <CommentList />
+    </div>
+  </div>
+);
+
 const ArticlePage = (initialArticle) => {
   const router = useRouter();
   const {
@@ -24,38 +57,26 @@ const ArticlePage = (initialArticle) => {
 
   const { article }: Article = fetchedArticle || initialArticle;
 
-  const markup = {
+  const htmlContent = {
     __html: marked(article.body, { sanitize: true }),
   };
 
   return (
     <div className="article-page">
-      <div className="banner">
-        <div className="container">
-          <h1>{article.title}</h1>
-          <ArticleMeta article={article} />
-        </div>
-      </div>
+
+      <ArticleBanner title={article.title} article={article} />
+
       <div className="container page">
         <div className="row article-content">
-          <div className="col-xs-12">
-            <div dangerouslySetInnerHTML={markup} />
-            <ul className="tag-list">
-              {article.tagList.map((tag) => (
-                <li key={tag} className="tag-default tag-pill tag-outline">
-                  {tag}
-                </li>
-              ))}
-            </ul>
-          </div>
+          <ArticleContent htmlContent={htmlContent} />
+          <TagList tags={article.tagList} />
         </div>
+        
         <hr />
+
         <div className="article-actions" />
-        <div className="row">
-          <div className="col-xs-12 col-md-8 offset-md-2">
-            <CommentList />
-          </div>
-        </div>
+        <CommentsSection />
+        
       </div>
     </div>
   );
