ccb5360c4295c5129d88b69c295e2218d78d0834
dpwls <dpwls02142@gmail.com>
2025-06-10 20:23:29 +0900

    refactor: Article api 타입 추가

--- lib/api/article.ts
+++ lib/api/article.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import { SERVER_BASE_URL } from '../utils/constant';
 import { getQuery } from '../utils/getQuery';
+import { ArticleType } from '../types/articleType';
 
 const article = `${SERVER_BASE_URL}/articles`;
-const withSlug = (slug) => `${article}/${encodeURIComponent(slug)}`;
-const withToken = (token) => ({
+const withSlug = (slug: string) => `${article}/${encodeURIComponent(slug)}`;
+const withToken = (token: string) => ({
   headers: {
     Authorization: `Token ${encodeURIComponent(token)}`,
     'Content-Type': 'application/json',
@@ -12,40 +13,41 @@ const withToken = (token) => ({
 });
 
 const ArticleAPI = {
-  all: (page, limit = 10) => axios.get(`${article}?${getQuery(limit, page)}`),
+  all: (page: number, limit: number = 10) =>
+    axios.get(`${article}?${getQuery(limit, page)}`),
 
-  byAuthor: (author, page = 0, limit = 5) =>
+  byAuthor: (author: string, page: number = 0, limit: number = 5) =>
     axios.get(
       `${article}?author=${encodeURIComponent(author)}&${getQuery(limit, page)}`,
     ),
 
-  byTag: (tag, page = 0, limit = 10) =>
+  byTag: (tag: string, page: number = 0, limit: number = 10) =>
     axios.get(
       `${article}?tag=${encodeURIComponent(tag)}&${getQuery(limit, page)}`,
     ),
 
-  delete: (slug, token) =>
+  delete: (slug: string, token: string) =>
     axios.delete(withSlug(slug), {
       headers: {
         Authorization: `Token ${encodeURIComponent(token)}`,
       },
     }),
 
-  favorite: (slug) => axios.post(`${withSlug(slug)}/favorite`),
+  favorite: (slug: string) => axios.post(`${withSlug(slug)}/favorite`),
 
-  favoritedBy: (author, page) =>
+  favoritedBy: (author: string, page: number) =>
     axios.get(
       `${article}?favorited=${encodeURIComponent(author)}&${getQuery(10, page)}`,
     ),
 
-  feed: (page, limit = 10) =>
+  feed: (page: number, limit: number = 10) =>
     axios.get(`${article}/feed?${getQuery(limit, page)}`),
 
-  get: (slug) => axios.get(withSlug(slug)),
+  get: (slug: string) => axios.get(withSlug(slug)),
 
-  unfavorite: (slug) => axios.delete(`${withSlug(slug)}/favorite`),
+  unfavorite: (slug: string) => axios.delete(`${withSlug(slug)}/favorite`),
 
-  update: async (article, token) => {
+  update: async (article: ArticleType, token: string) => {
     const { data, status } = await axios.put(
       withSlug(article.slug),
       JSON.stringify({ article }),
@@ -54,13 +56,13 @@ const ArticleAPI = {
     return { data, status };
   },
 
-  create: async (articleData, token) => {
-      const { data, status } = await axios.post(
-        article,
-        JSON.stringify({ article: articleData }),
-        withToken(token),
-      );
-      return { data, status };
+  create: async (articleData: Partial<ArticleType>, token: string) => {
+    const { data, status } = await axios.post(
+      article,
+      JSON.stringify({ article: articleData }),
+      withToken(token),
+    );
+    return { data, status };
   },
 };
 
