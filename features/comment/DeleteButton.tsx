eb653f6fabe5bee068f7552dfa477b3b8d9c7a53
dpwls <130109502+dpwls02142@users.noreply.github.com>
2025-06-13 17:03:32 +0900

    feat: 댓글 삭제시 5초동안 되돌리기 생성 (#18)
    * refactor: add auth for comment api
    
    * refactor: comment api에서 delete 갖고오기
    
    * feat: add comment delete confirm action
    
    * feat: add delete comment cancel
    
    * refactor: add magic number to delete comment undo

--- features/comment/DeleteButton.tsx
+++ features/comment/DeleteButton.tsx
@@ -1,32 +1,60 @@
-import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import useSWR, { trigger } from 'swr';
+import { trigger } from 'swr';
 
 import { SERVER_BASE_URL } from '../../lib/utils/constant';
-import storage from '../../lib/utils/storage';
+import CommentAPI from '../../lib/api/comment';
 
-const DeleteButton = ({ commentId }) => {
-  const { data: currentUser } = useSWR('user', storage);
+const INITIAL_COUNTDOWN_SECONDS = 5;
+const COUNTDOWN_INTERVAL_MS = 1000;
+
+const DeleteButton = ({ commentId }: { commentId: string }) => {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [countdown, setCountdown] = useState(INITIAL_COUNTDOWN_SECONDS);
   const router = useRouter();
+
   const {
     query: { slug },
   } = router;
 
-  const handleDelete = async (commentId) => {
-    await axios.delete(
-      `${SERVER_BASE_URL}/articles/${slug}/comments/${commentId}`,
-      {
-        headers: {
-          Authorization: `Token ${currentUser?.token}`,
-        },
-      },
-    );
-    trigger(`${SERVER_BASE_URL}/articles/${slug}/comments`);
-  };
+  useEffect(() => {
+    if (!isDeleting) return;
+
+    if (countdown === 0) {
+      CommentAPI.delete(slug as string, commentId).then(() => {
+        trigger(`${SERVER_BASE_URL}/articles/${slug}/comments`);
+        setIsDeleting(false);
+      });
+      return;
+    }
+
+    const commentDeleteTimer = setTimeout(() => {
+      setCountdown(countdown - 1);
+    }, COUNTDOWN_INTERVAL_MS);
+
+    return () => clearTimeout(commentDeleteTimer);
+  }, [isDeleting, countdown, slug, commentId]);
+
+  function DeleteComment() {
+    setIsDeleting(true);
+    setCountdown(INITIAL_COUNTDOWN_SECONDS);
+  }
+
+  function DeleteCancel() {
+    setIsDeleting(false);
+    setCountdown(INITIAL_COUNTDOWN_SECONDS);
+  }
 
   return (
     <span className="mod-options">
-      <i className="ion-trash-a" onClick={() => handleDelete(commentId)} />
+      {!isDeleting ? (
+        <i className="ion-trash-a" onClick={DeleteComment} />
+      ) : (
+        <div>
+          <span>{countdown}초 후 삭제</span>
+          <i className="ion-arrow-return-left" onClick={DeleteCancel} />
+        </div>
+      )}
     </span>
   );
 };
