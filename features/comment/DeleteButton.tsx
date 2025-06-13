3e5619f872b42f15ce46e6814a972002486b2303
dpwls <dpwls02142@gmail.com>
2025-06-13 16:54:05 +0900

    feat: add delete comment cancel

--- features/comment/DeleteButton.tsx
+++ features/comment/DeleteButton.tsx
@@ -1,29 +1,57 @@
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { trigger } from 'swr';
 
 import { SERVER_BASE_URL } from '../../lib/utils/constant';
 import CommentAPI from '../../lib/api/comment';
 
-const CONFIRM_DELETE_MESSAGE = `Do you really want to delete it?`;
-
 const DeleteButton = ({ commentId }: { commentId: string }) => {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [countdown, setCountdown] = useState(5);
   const router = useRouter();
+
   const {
     query: { slug },
   } = router;
 
-  const handleDelete = async (commentId: string) => {
-    const result = window.confirm(CONFIRM_DELETE_MESSAGE);
+  useEffect(() => {
+    if (!isDeleting) return;
 
-    if (!result) return;
+    if (countdown === 0) {
+      CommentAPI.delete(slug as string, commentId).then(() => {
+        trigger(`${SERVER_BASE_URL}/articles/${slug}/comments`);
+        setIsDeleting(false);
+      });
+      return;
+    }
 
-    await CommentAPI.delete(slug as string, commentId);
-    trigger(`${SERVER_BASE_URL}/articles/${slug}/comments`);
-  };
+    const commentDeleteTimer = setTimeout(() => {
+      setCountdown(countdown - 1);
+    }, 1000);
+
+    return () => clearTimeout(commentDeleteTimer);
+  }, [isDeleting, countdown, slug, commentId]);
+
+  function DeleteComment() {
+    setIsDeleting(true);
+    setCountdown(5);
+  }
+
+  function DeleteCancel() {
+    setIsDeleting(false);
+    setCountdown(5);
+  }
 
   return (
     <span className="mod-options">
-      <i className="ion-trash-a" onClick={() => handleDelete(commentId)} />
+      {!isDeleting ? (
+        <i className="ion-trash-a" onClick={DeleteComment} />
+      ) : (
+        <div>
+          <span>{countdown}초 후 삭제</span>
+          <i className="ion-arrow-return-left" onClick={DeleteCancel} />
+        </div>
+      )}
     </span>
   );
 };
