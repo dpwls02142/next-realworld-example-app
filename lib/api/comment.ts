e0eb74295fa41a8c6af5908f95c783b045188a85
dpwls <dpwls02142@gmail.com>
2025-06-13 15:52:01 +0900

    refactor: add auth for comment api

--- lib/api/comment.ts
+++ lib/api/comment.ts
@@ -1,13 +1,23 @@
 import axios from 'axios';
 
 import { SERVER_BASE_URL } from '../utils/constant';
+import storage from '../utils/storage';
+
+const getAuthHeader = async () => {
+  const currentUser = await storage('user');
+  return {
+    Authorization: `Token ${currentUser?.token}`,
+  };
+};
 
 const CommentAPI = {
   create: async (slug: string, comment: string) => {
     try {
+      const headers = await getAuthHeader();
       const response = await axios.post(
         `${SERVER_BASE_URL}/articles/${slug}/comments`,
         JSON.stringify({ comment }),
+        { headers },
       );
       return response;
     } catch (error) {
@@ -16,8 +26,10 @@ const CommentAPI = {
   },
   delete: async (slug: string, commentId: string) => {
     try {
+      const headers = await getAuthHeader();
       const response = await axios.delete(
         `${SERVER_BASE_URL}/articles/${slug}/comments/${commentId}`,
+        { headers },
       );
       return response;
     } catch (error) {
@@ -25,8 +37,12 @@ const CommentAPI = {
     }
   },
 
-  forArticle: (slug: string) =>
-    axios.get(`${SERVER_BASE_URL}/articles/${slug}/comments`),
+  forArticle: async (slug: string) => {
+    const headers = await getAuthHeader();
+    return axios.get(`${SERVER_BASE_URL}/articles/${slug}/comments`, {
+      headers,
+    });
+  },
 };
 
 export default CommentAPI;
