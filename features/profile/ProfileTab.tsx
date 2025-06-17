b532a7cea72d8ace3bfd659ad836dd4a5580a01c
dpwls <dpwls02142@gmail.com>
2025-06-17 19:37:54 +0900

    fix: tabList CustomLink 컴포넌트 사용

--- features/profile/ProfileTab.tsx
+++ features/profile/ProfileTab.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
-import NavLink from '../../shared/ui/layout/NavLink';
+import CustomLink from '../../shared/components/CustomLink';
 import { usePageDispatch } from '../../lib/context/PageContext';
 
 const ProfileTab = ({ profile }) => {
   const setPage = usePageDispatch();
+  const router = useRouter();
+  const {
+    query: { favorite },
+  } = router;
   const encodedUsername = encodeURIComponent(profile.username);
 
   return (
     <ul className="nav nav-pills outline-active">
       <li className="nav-item">
-        <NavLink href="/profile/[pid]" as={`/profile/${encodedUsername}`}>
+        <CustomLink
+          href="/profile/[pid]"
+          as={`/profile/${encodedUsername}`}
+          className={`nav-link ${!favorite ? 'active' : ''}`}
+        >
           <span onClick={() => setPage(0)}>My Articles</span>
-        </NavLink>
+        </CustomLink>
       </li>
       <li className="nav-item">
-        <NavLink
+        <CustomLink
           href="/profile/[pid]?favorite=true"
           as={`/profile/${encodedUsername}?favorite=true`}
+          className={`nav-link ${favorite ? 'active' : ''}`}
         >
           <span onClick={() => setPage(0)}>Favorited Articles</span>
-        </NavLink>
+        </CustomLink>
       </li>
     </ul>
   );
